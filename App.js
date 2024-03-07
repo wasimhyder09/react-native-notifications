fd38659c16978feef7a0a1beb9b1c36c2c636f52
Wasim Hyder <wasim@creativefaze.com>
2024-03-07 23:17:28 +0500

    Displayed notification

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true
+    };
+  }
+});
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    Notifications.scheduleNotificationAsync({
+      trigger: {
+        seconds: 5
+      },
+      content: {
+        title: 'My first local notification',
+        body: 'This is body of notification',
+        data: { userName: 'Wasim' }
+      },
+
+    });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Schedule Notification' onPress={scheduleNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
