8916a00edb2af6a33d74b0ed4bc5db196c09f56c
Wasim Hyder <wasim@creativefaze.com>
2024-03-08 21:39:50 +0500

    Added push notifications via api

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import * as Notifications from 'expo-notifications';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, View, Platform } from 'react-native';
 import { useEffect } from 'react';
 
 Notifications.setNotificationHandler({
@@ -15,16 +15,46 @@ Notifications.setNotificationHandler({
 
 export default function App() {
   useEffect(() => {
+    async function setupPushNotifications() {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (finalStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== 'granted') {
+        Alert.alert('Permission required', 'Push notifications need the appropriate permissions');
+        return;
+      }
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+      console.log(pushTokenData);
+
+      if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.DEFAULT
+        });
+      }
+      console.log('Taha');
+      console.log(finalStatus);
+    }
+
+    setupPushNotifications();
+
+  }, []);
+
+  useEffect(() => {
     const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
-      console.log('notification received');
-      console.log(notification);
+      // console.log('notification received');
+      // console.log(notification);
       const username = notification.request.content.data.userName;
-      console.log(username);
+      // console.log(username);
     });
 
     const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
-      console.log('notification response received');
-      console.log(response);
+      // console.log('notification response received');
+      // console.log(response);
     });
 
     return () => {
